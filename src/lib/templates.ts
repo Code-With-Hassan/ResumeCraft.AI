44d403c94836ea7d9c5ab311ef592772ce429a4d
Muhammad Hassan Shafique <hassanshafiq2011@gmail.com>
2025-10-14 17:26:35 +0000

    Add a new template with following markdown
    # John Smith
    
    **Marketing M

--- src/lib/templates.ts
+++ src/lib/templates.ts
@@ -64,7 +64,7 @@ export const templates: Template[] = [
 - {{skills}}
     `,
   },
-  {
+    {
     id: 'corporate',
     name: 'Corporate',
     isPremium: false,
@@ -132,5 +132,45 @@ export const templates: Template[] = [
 ### **Skills**
 > {{skills}}
     `
+  },
+  {
+    id: 'professional',
+    name: 'Professional',
+    isPremium: false,
+    markdown: `
+# {{name}}
+> {{email}} | {{phone}} | {{website}}
+
+***
+
+## Experience
+{{#experience}}
+**{{title}} at {{company}}**
+*{{startDate}} – {{endDate}}*
+
+* {{description}}
+{{/experience}}
+
+***
+
+## Projects
+
+**Brand Revival Campaign**
+* Developed and executed a campaign for a clothing brand that resulted in a 50% increase in website traffic and a 30% in online sales.
+* Managed influencer partnerships and social media strategy to connect with the target audience.
+
+***
+
+## Skills
+> {{skills}}
+
+***
+
+## Education
+{{#education}}
+**{{degree}}**
+{{institution}}, {{startDate}} – {{endDate}}
+{{/education}}
+`
   }
 ];
